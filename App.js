c45c1782e7b03a9d200c5c9b3cd2c991072b5578
HerlockSholmess <23009662@myrp.edu.sg>
2024-10-28 15:47:08 +0800

    weqw

--- App.js
+++ App.js
@@ -1,20 +1,108 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, TextInput, View, Button, Alert, TouchableOpacity, ToastAndroid, Image,ScrollView} from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import RNPickerSelect from 'react-native-picker-select';
+import React, {useState}  from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App=() => {
+    const [pw, SetPw] = useState("");
+    const [pd, SetPd] = useState("");
+    const [pe, SetPe] = useState("");
+    return (
+        <ScrollView style={styles.container}>
+            <Image
+                source={require('./img/Question1.png')}
+                style={{ width: 400, height: 200 }}
+                resizeMode="contain"
+            />
+            <RNPickerSelect
+                onValueChange={(value) => SetPw(value)}
+                items={[
+                    { label: '24', value: '24' },
+                    { label: '10', value: '10' },
+                    { label: '11', value: '11' },
+
+                ]}
+            />
+            <Text>What is the answer?</Text>
+            <Image
+                source={require('./img/Question2.png')}
+                style={{ width: 400, height: 200 }}
+                resizeMode="contain"
+            />
+            <RNPickerSelect
+                onValueChange={(value) => SetPd(value)}
+                items={[
+                    { label: '20/9', value: '20/9' },
+                    { label: '2.22', value: '2.22' },
+                    { label: '2 2/9', value: '2 2/9' },
+
+                ]}
+            />
+            <Text>What is the answer?</Text>
+
+            <Image
+                source={require('./img/Question3.png')}
+                style={{ width: 400, height: 200 }}
+                resizeMode="contain"
+            />
+            <RNPickerSelect
+                onValueChange={(value) => SetPe(value)}
+                items={[
+                    { label: '-16288', value: '-16288' },
+                    { label: '-1056', value: '-1056' },
+                    { label: '-1101', value: ' -1101' },
+
+                ]}
+            />
+            <Text>What is the answer?</Text>
+
+
+
+            <Button onPress={() => {
+                const SetPw = '10'
+                const SetPd = '2 2/9'
+                const SetPe = '-16288'
+                let message = 'No answer was entered'
+                let score = 0
+                if (pw === SetPw){
+                    score += 1;
+                }
+                if (pd === SetPd){
+                    score += 1;
+                }
+                if (pe === SetPe){
+                    score += 1;
+                }
+                message = 'You have' + score + 'correct answers'
+                Alert.alert(message)
+            }}
+                title="Submit answers" color="cadetblue"
+            />
+
+
+
+
+            <StatusBar hidden={true} />
+        </ScrollView>
+    );
+};
+
+export default App;
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+
+
+    },
 });
+
+
+
+
+
+
+
+
